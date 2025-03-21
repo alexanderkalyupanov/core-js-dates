236f5f19457f6085724253071a93dbd4ef4d0c38
Alexander Kalyupanov <alexander.kalyupanov@mail.ru>
2025-03-21 16:36:50 +0300

    feat: complete task getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -106,8 +106,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const firstDate = new Date(dateStart);
+  const secondDate = new Date(dateEnd);
+  return (secondDate - firstDate) / 86400000 + 1;
 }
 
 /**
